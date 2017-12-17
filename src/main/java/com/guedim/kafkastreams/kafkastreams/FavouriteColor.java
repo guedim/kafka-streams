94c675e3f298b8f142cbb8beee5d58f7ace00339
Mario Guerrero <guedim@gmail.com>
2017-12-17 14:04:23 -0500

    ajustes docker-compose

--- src/main/java/com/guedim/kafkastreams/kafkastreams/FavouriteColor.java
+++ src/main/java/com/guedim/kafkastreams/kafkastreams/FavouriteColor.java
@@ -15,17 +15,27 @@ import org.apache.kafka.streams.kstream.KTable;
 public class FavouriteColor {
 
 	private static final String APPLICATION_ID = "favourite-color-app";
-	private static final String SERVER_CONFIG = "127.0.0.1:9092";
+	private static final String SERVER_CONFIG_HOST = "127.0.0.1";
+	private static final String SERVER_CONFIG_PORT = "9092";
 	private static final String RESET_CONFIG = "earliest";
 	private static final String INPUT_TOPIC = "fav-color-input";
 	private static final String MIDDLE_TOPIC = "fav-color-middle";
 	private static final String OUTPUT_TOPIC = "fav-color-output";
 	private static final String SPLIT_CHARACTER = ",";
+	private static final String ADVERTISED_HOST = "ADVERTISED_HOST";
 
 	public static void favourtiteColor() {
+		
+		String hostProperty = System.getProperty(ADVERTISED_HOST);
+		String hostEnvironm = System.getenv(ADVERTISED_HOST);
+		String host = hostProperty!=null?hostProperty:hostEnvironm!=null?hostEnvironm:SERVER_CONFIG_HOST;
+		System.out.println("Host Property:" + hostProperty);
+		System.out.println("Host Environm:" + hostEnvironm);
+		System.out.println("Advertised host:" + host);
+		
 		Properties config = new Properties();
 		config.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
-		config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, SERVER_CONFIG);
+		config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, host+":"+SERVER_CONFIG_PORT);
 		config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, RESET_CONFIG);
 		config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 		config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
