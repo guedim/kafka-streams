b2a771278185f65e3dab1c499362d63d46936f15
Mario Guerrero <guedim@gmail.com>
2017-12-19 22:29:10 -0500

    se ajuste el bank transfer

--- src/main/java/com/guedim/kafkastreams/kafkastreams/bank/BankTransactionsProducer.java
+++ src/main/java/com/guedim/kafkastreams/kafkastreams/bank/BankTransactionsProducer.java
@@ -10,37 +10,38 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import static com.guedim.kafkastreams.kafkastreams.bank.Properties.*;
 
 public class BankTransactionsProducer {
-  public static void main(String[] args) {
 
-    Properties properties = new Properties();
-    // kafka bootstrap server
-    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getServerConfig());
-    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    // producer acks
-    properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // strongest producing guarantee
-    properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
-    properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
-    // leverage idempotent producer from Kafka 0.11 !
-    properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true"); // ensure we don't push duplicates
+	public static void main(String[] args) {
 
-    Producer<String, String> producer = new KafkaProducer<>(properties);
+		Properties properties = new Properties();
+		// kafka bootstrap server
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getServerConfig());
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		// producer acks
+		properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // strongest producing guarantee
+		properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
+		properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
+		// leverage idempotent producer from Kafka 0.11 !
+		properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true"); // ensure we don't push duplicates
 
-    int i = 0;
-    while (true) {
-      System.out.println("Producing batch: " + i);
-      try {
-        producer.send(ProducerRecordModel.newRandomTransaction("john"));
-        Thread.sleep(100);
-        producer.send(ProducerRecordModel.newRandomTransaction("stephane"));
-        Thread.sleep(100);
-        producer.send(ProducerRecordModel.newRandomTransaction("alice"));
-        Thread.sleep(100);
-        i += 1;
-      } catch (InterruptedException e) {
-        break;
-      }
-    }
-    producer.close();
-  }
+		Producer<String, String> producer = new KafkaProducer<>(properties);
+
+		int i = 0;
+		while (true) {
+			System.out.println("Producing batch: " + i);
+			try {
+				producer.send(ProducerRecordModel.newRandomTransaction("john"));
+				Thread.sleep(100);
+				producer.send(ProducerRecordModel.newRandomTransaction("stephane"));
+				Thread.sleep(100);
+				producer.send(ProducerRecordModel.newRandomTransaction("alice"));
+				Thread.sleep(100);
+				i += 1;
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+		producer.close();
+	}
 }
